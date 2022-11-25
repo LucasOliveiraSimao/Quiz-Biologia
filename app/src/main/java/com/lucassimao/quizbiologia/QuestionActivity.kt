3ea0e1cb91538869e7effe760b9595ffc6d073ce
Lucas <oliveira.lucas20@gmail.com>
2022-11-25 17:08:01 -0400

    Changes filenames Main To Question

--- app/src/main/java/com/lucassimao/quizbiologia/QuestionActivity.kt
+++ app/src/main/java/com/lucassimao/quizbiologia/QuestionActivity.kt
@@ -3,9 +3,9 @@ package com.lucassimao.quizbiologia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class QuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_question)
     }
 }
